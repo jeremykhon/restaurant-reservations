8d1aeb64bc8ccd7671c49be8216b481a8205533f
Jeremy Hon <jehon11@gmail.com>
2019-05-08 16:59:41 +0900

    creating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: [ :index, :show, :create ] do
+        resources :default_time_slots, only: [ :index, :create ]
+        resources :time_slots, only: [ :index ]
+      end
+    end
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
