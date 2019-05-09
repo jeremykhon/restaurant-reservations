4069335562e6b6f3acef7af31353d411888b423b
Jeremy Hon <jehon11@gmail.com>
2019-05-09 16:00:23 +0900

    added has many assoc to restaurant for timeslots

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   belongs_to :cuisine
+  has_many :time_slots
+  has_many :reviews
+  has_many :restaurant_photos
 
   validates :user, :cuisine, :name, :location, :capacity, :booking_window, presence: true
 end
