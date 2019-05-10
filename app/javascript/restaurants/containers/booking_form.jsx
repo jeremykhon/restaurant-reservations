b3ef2f7fb1e2e02fc8eb8091c9a41af5e8848936
Jeremy Hon <jehon11@gmail.com>
2019-05-10 15:32:49 +0900

    implemented form

--- app/javascript/restaurants/containers/booking_form.jsx
+++ app/javascript/restaurants/containers/booking_form.jsx
@@ -3,15 +3,58 @@ import React, { Component } from 'react';
 const BASE_URL = '/api/v1';
 
 class BookingForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      date: '',
+      tableSize: '',
+      timeSlotId: '',
+      name: '',
+      email: '',
+      number: '',
+    };
+  }
 
-  componentDidMount() {
+  handleChange = (event) => {
+    const target = event.target
+    const name = target.name;
+    this.setState({[name]: target.value});
+  }
+
+  handleSubmit = (event) => {
+    alert('A name was submitted: ' + this.state.date);
+    event.preventDefault();
   }
 
   render() {
     return (
-      <div>
-        bookhere
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Date:
+          <input type="date" name="date" value={this.state.date} onChange={this.handleChange} />
+        </label>
+        <label>
+          Number of People:
+          <input type="number" name="tableSize" value={this.state.tableSize} onChange={this.handleChange} />
+        </label>
+        <label>
+          TimeSlot:
+          <input type="text" name="timeSlotId" value={this.state.timeSlotId} onChange={this.handleChange} />
+        </label>
+        <label>
+          Name:
+          <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <label>
+          Email:
+          <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+        </label>
+        <label>
+          Number:
+          <input type="text" name="number" value={this.state.number} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
