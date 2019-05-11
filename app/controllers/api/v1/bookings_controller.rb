b22a69925673d96caa0e0958bb0521f8010d52c4
Jeremy Hon <jehon11@gmail.com>
2019-05-11 18:59:00 +0900

    inputing params to bookings controller

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -5,7 +5,18 @@ class Api::V1::BookingsController < ApplicationController
   end
 
   def create
-    puts params
+    booking = Booking.new(date: params[:date],
+                          table_size: params[:tableSize],
+                          time_slot_id: params[:selectedTimeSlot][:id],
+                          restaurant_id: params[:selectedTimeSlot][:restaurant_id],
+                          time: Time.parse(params[:selectedTimeSlot][:time]),
+                          name: params[:name],
+                          email: params[:email],
+                          number: params[:number],
+                          discount: params[:selectedTimeSlot][:discount])
+    booking.user = current_user if user_signed_in?
+    p "$$$$$$$$$$$$$$"
+    p booking
     # restaurant = Restaurant.new(name: params[:name])
     # restaurant.save
     # render json: channel
