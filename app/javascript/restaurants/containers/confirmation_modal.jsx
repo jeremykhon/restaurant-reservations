3c7b3d6a90c6d65a8358553f99601e74e5038403
Jeremy Hon <jehon11@gmail.com>
2019-05-12 01:44:52 +0900

    can display confirmed page after user confirms booking

--- app/javascript/restaurants/containers/confirmation_modal.jsx
+++ app/javascript/restaurants/containers/confirmation_modal.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import hhmmTime from '../utils/hhmm_time';
 
 const BASE_URL = '/api/v1';
@@ -28,19 +29,36 @@ class ConfirmationModal extends Component {
       .then(data => this.setState({booking: data, confirmed: true}));;
   }
 
+  result = () => {
+    const {bookingForm, closeModal} = this.props
+    if (this.state.confirmed) {
+      return (
+        <div>
+          <div>confirmed!</div>
+          <Link to={`/`}>Keep Browsing</Link>
+        </div>
+      )
+    } else {
+      return (  
+        <div>
+          <div>{bookingForm.date}</div>
+          <div>{bookingForm.tableSize}</div>
+          <div>{hhmmTime(bookingForm.selectedTimeSlot.time)}</div>
+          <div>{bookingForm.selectedTimeSlot.discount}</div>
+          <div>{bookingForm.name}</div>
+          <div>{bookingForm.email}</div>
+          <div>{bookingForm.number}</div>
+          <button type="button" onClick={this.createBooking}>Confirm</button>
+          <button type="button" onClick={closeModal}>Cancel</button>
+        </div>
+      )
+    }
+  }
+
   render() {
-    const { bookingForm, closeModal } = this.props
     return (
       <div>
-        <div>{bookingForm.date}</div>
-        <div>{bookingForm.tableSize}</div>
-        <div>{hhmmTime(bookingForm.selectedTimeSlot.time)}</div>
-        <div>{bookingForm.selectedTimeSlot.discount}</div>
-        <div>{bookingForm.name}</div>
-        <div>{bookingForm.email}</div>
-        <div>{bookingForm.number}</div>
-        <button type="button" onClick={this.createBooking}>Confirm</button>
-        <button type="button" onClick={closeModal}>Cancel</button>
+        {this.result()}
       </div>
     );
   }
