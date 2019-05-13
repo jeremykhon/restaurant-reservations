151d71f810a1518040ce98e311359740173e629b
Jeremy Hon <jehon11@gmail.com>
2019-05-13 19:25:31 +0900

    can show log in when jwt not present

--- app/javascript/restaurants/containers/navbar.jsx
+++ app/javascript/restaurants/containers/navbar.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LoggedIn = (jwt) => {
+  if (jwt) {
+    return (
+      <div className="nav-link">
+        Logout
+      </div>
+    );
+  }
+  return (
+    <div className="nav-bar-right">
+      <div className="nav-link">
+        login
+      </div>
+      <div className="nav-link">
+        sign up
+      </div>
+    </div>
+  );
+};
 
 const Navbar = () => {
   return (
     <div className="container-fluid nav-bar">
-      <div>
-        logo
+      <div className="nav-bar-left">
+        <div>
+          <Link exact className="nav-link" to="/">
+            Home
+          </Link>
+        </div>
       </div>
-      <div>
-        login
-      </div>
-      <div>
-        sign up
-      </div>
-      <div>
-        log out
+      <div className="nav-bar-right">
+        {LoggedIn(localStorage.getItem('jwt'))}
       </div>
     </div>
   );
