8d5a766c0ef4721e7618715820cf33b7d7394370
Jeremy Hon <jehon11@gmail.com>
2019-05-14 17:22:59 +0900

    create user controller logic

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,5 +2,11 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :authenticate_request, only: [:create]
 
   def create
+    user = User.new(email: params[:email], name: params[:name], password: params[:password])
+    if user.save!
+      render json: { message: "user created!" }
+    else
+      render json: { message: "something went wrong" }, status: :internal_server_error
+    end
   end
 end
