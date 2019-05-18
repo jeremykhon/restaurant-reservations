e8b836ed37c6b39aef68db9278e34997ac0c7f4a
Jeremy Hon <jehon11@gmail.com>
2019-05-18 18:02:07 +0900

    styled reservations card

--- app/javascript/restaurants/components/reservation_card.jsx
+++ app/javascript/restaurants/components/reservation_card.jsx
@@ -8,23 +8,38 @@ import RestaurantPhoto from './restaurant_photo';
 const ReservationCard = ({ reservation }) => {
   return (
     <div className="reservation-card">
-      <div className="reservation-restaurant-photo-container">
-        <RestaurantPhoto optionalClass="reservation-photo" photo={reservation.restaurant.restaurant_photos[0]} />
+      <div className="reservation-card-left">
+        <div className="reservation-card-photo-container">
+          <RestaurantPhoto optionalClass="reservation-photo" photo={reservation.restaurant.restaurant_photos[0]} />
+        </div>
+        <button type="button" className="view-restaurant-button no-select">view restaurant</button>
       </div>
-      <div>
-        {reservation.restaurant.name}
-      </div>
-      <div>
-        {longDate(reservation.time)}
-      </div>
-      <div>
-        {hhmmTime(reservation.time)}
-      </div>
-      <div>
-        {`${reservation.table_size} people`}
-      </div>
-      <div>
-        {reservation.discount}
+      <div className="reservation-card-right">
+        <div className="reservation-card--restaurant-name">
+          {reservation.restaurant.name}
+        </div>
+        <div className="reservation-card-content">
+          <div className="reservation-card-content-left">
+            <div className="reservation-card--detail">
+              <i className="far fa-calendar-alt icon" />
+              {longDate(reservation.time)}
+            </div>
+            <div className="reservation-card--detail">
+              <i className="far fa-clock icon" />
+              {hhmmTime(reservation.time)}
+            </div>
+          </div>
+          <div className="reservation-card-content-right">
+            <div className="reservation-card--detail">
+              <i className="fas fa-user-friends icon" />
+              {`${reservation.table_size} people`}
+            </div>
+            <div className="reservation-card--detail">
+              <i className="fas fa-tag icon" />
+              {reservation.discount}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
