bb86deb44f86b161a4edb40e49d47a0020af2d1c
Jeremy Hon <jehon11@gmail.com>
2019-05-18 14:33:45 +0900

    can show restaurant photos in grid format

--- app/javascript/restaurants/components/restaurant_photos.jsx
+++ app/javascript/restaurants/components/restaurant_photos.jsx
@@ -1,29 +1,45 @@
+/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import axios from "axios";
 import BASE_URL from '../utils/base_url';
 
-class RestaurantPhotos extends Component {
-  constructor() {
-    super();
-    this.state = {
-      photos: []
-    };
-  }
-
-  componentDidMount() {
-    const { restaurant } = this.props;
-    axios.get(`${BASE_URL}/restaurants/${restaurant.id}/restaurant_photos`)
-      .then(response => console.log(response.data));
-  }
-
-  render() {
+const RestaurantPhoto = ({ photo }) => {
+  if (photo) {
     return (
-      <div>
-        photos
+      <div className="restaurant-photo" style={{ backgroundImage: `url(${photo.photo.url})` }} />
+    );
+  }
+  return (
+    <div className="restaurant-no-photo" />
+  );
+};
+
+class RestaurantPhotos extends Component {
+  render() {
+    const { photos } = this.props;
+    return (
+      <div className="restaurant-photos-container">
+        <div className="restaurant-photos-left">
+          <div className="photo-1">
+            <RestaurantPhoto photo={photos[0]} />
+          </div>
+        </div>
+        <div className="restaurant-photos-right">
+          <div className="photo-2">
+            <RestaurantPhoto photo={photos[1]} />
+          </div>
+          <div className="photo-3">
+            <RestaurantPhoto photo={photos[2]} />
+          </div>
+          <div className="photo-4">
+            <RestaurantPhoto photo={photos[3]} />
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 export default RestaurantPhotos;
 
 // this.setState({ photos: response.data }
