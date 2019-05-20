31f03bedbe584b8fb8590518bc1be5f67d4eebf0
Jeremy Hon <jehon11@gmail.com>
2019-05-20 21:06:54 +0900

    added average rating column to restaurant

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ReviewsController < ApplicationController
   skip_before_action :authenticate_request, only: :index
+  before_action :set_restaurant, only: :create
 
   def index
     # reviews = Review.where(restaurant_id: params[:restaurant_id]).order(created_at: :desc)
@@ -8,24 +9,28 @@ class Api::V1::ReviewsController < ApplicationController
 
   def create
     review = Review.new(
-      restaurant_id: params[:restaurant_id],
+      restaurant: @restaurant,
       rating: params[:rating],
       content: params[:content],
       user: current_user
     )
     if review.save!
-      
+      calc_average_rating
       render json: review, include: :user
     end
   end
 
   private
 
-  def recalc_average_rating
-    restaurant = Restaurant.find(params[:restaurant_id])
+  def calc_average_rating
+    reviews = @restaurant.reviews
     sum = 0
-    reviews = restaurant.reviews
     reviews.each { |review| sum += review.rating }
-    restaurant.avg_rating = sum.fdiv(reviews)
+    @restaurant.avg_rating = sum.fdiv(reviews.length)
+    @restaurant.save!
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_054122) do
+ActiveRecord::Schema.define(version: 2019_05_20_115512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2019_05_16_054122) do
     t.integer "booking_window"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "avg_rating", default: 0.0
     t.index ["cuisine_id"], name: "index_restaurants_on_cuisine_id"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
