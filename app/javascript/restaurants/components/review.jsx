13e1ad8054a6e5d7295751c21e0675cdce6a7f3f
Jeremy Hon <jehon11@gmail.com>
2019-05-20 17:58:20 +0900

    added star svg

--- app/javascript/restaurants/components/review.jsx
+++ app/javascript/restaurants/components/review.jsx
@@ -8,6 +8,11 @@ const Review = ({ review }) => {
         <div className="review-user-name">{review.user.name}</div>
         <div className="review-date">{MmmDdYyyy(review.created_at)}</div>
       </div>
+      <div className="star-container">
+        <svg viewBox="0 0 50 50" className="star-svg">
+          <path className="star" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" />
+        </svg>
+      </div>
       <div className="review-rating">{review.rating}</div>
       <div className="review-content">{review.content}</div>
     </div>
