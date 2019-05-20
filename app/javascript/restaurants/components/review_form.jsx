ee25530601cacc443cbebe2a9fbc0d165924a9e1
Jeremy Hon <jehon11@gmail.com>
2019-05-20 19:33:48 +0900

    styled review form

--- app/javascript/restaurants/components/review_form.jsx
+++ app/javascript/restaurants/components/review_form.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import BASE_URL from '../utils/base_url';
 
+const starSvg = "m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"
+
 class ReviewForm extends Component {
   constructor() {
     super();
     this.state = {
-      rating: '',
+      rating: 0,
       content: '',
     };
   }
@@ -43,21 +45,46 @@ class ReviewForm extends Component {
       .catch(error => console.log(error));
   }
 
+  updateRating = (rating) => {
+    this.setState({ rating });
+  }
+
+  starFill = (starPosition) => {
+    const { rating } = this.state;
+    if (rating >= starPosition) {
+      return 'star-fill';
+    }
+  };
 
   render() {
     const { rating, content } = this.state;
+    const { closeModal } = this.props;
     return (
-      <div>
+      <div className="review-form-container">
+        <div className="form-title">Leave a review</div>
         <form onSubmit={this.handleSubmit}>
-          <div>
-            rating
-            <input type="text" name="rating" value={rating} onChange={this.handleChange} />
+          <div className="star-container-center">
+            <svg viewBox="0 0 50 50" className="star-svg-clickable" onClick={() => { this.updateRating(1); }}>
+              <path className={`star ${this.starFill(1)}`} d={starSvg} />
+            </svg>
+            <svg viewBox="0 0 50 50" className="star-svg-clickable" onClick={() => { this.updateRating(2); }}>
+              <path className={`star ${this.starFill(2)}`} d={starSvg} />
+            </svg>
+            <svg viewBox="0 0 50 50" className="star-svg-clickable" onClick={() => { this.updateRating(3); }}>
+              <path className={`star ${this.starFill(3)}`} d={starSvg} />
+            </svg>
+            <svg viewBox="0 0 50 50" className="star-svg-clickable" onClick={() => { this.updateRating(4); }}>
+              <path className={`star ${this.starFill(4)}`} d={starSvg} />
+            </svg>
+            <svg viewBox="0 0 50 50" className="star-svg-clickable" onClick={() => { this.updateRating(5); }}>
+              <path className={`star ${this.starFill(5)}`} d={starSvg} />
+            </svg>
           </div>
-          <div>
-            content
-            <input type="text" name="content" value={content} onChange={this.handleChange} />
+          <div className="form-item">
+            <textarea className="review-form-comment-box form-text-area no-select" wrap="soft" name="content" value={content} placeholder="please share your experience" onChange={this.handleChange} />
           </div>
-          <button type="submit" value="Submit">Submit</button>
+          <button className="form-submit no-select" type="submit" value="Submit">Submit</button>
+          <button className="form-cancel no-select" type="button" onClick={closeModal}>Cancel</button>
         </form>
       </div>
     );
