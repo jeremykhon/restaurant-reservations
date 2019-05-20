7c97462b3d2b4eafcfef60a25b9c5e382c9d2d9f
Jeremy Hon <jehon11@gmail.com>
2019-05-20 16:52:32 +0900

    basic styling on reviews

--- app/javascript/restaurants/components/review.jsx
+++ app/javascript/restaurants/components/review.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import MmmDdYyyy from '../utils/mmm_dd_yyyy';
 
 const Review = ({ review }) => {
   return (
-    <div>
-      <div>{review.user.name}</div>
-      <div>{review.content}</div>
-      <div>{review.rating}</div>
-      <div>{review.created_at}</div>
+    <div className="review-container">
+      <div className="review-top">
+        <div className="review-user-name">{review.user.name}</div>
+        <div className="review-date">{MmmDdYyyy(review.created_at)}</div>
+      </div>
+      <div className="review-rating">{review.rating}</div>
+      <div className="review-content">{review.content}</div>
     </div>
   );
 };
