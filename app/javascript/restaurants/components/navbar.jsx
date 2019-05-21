c47bc0ffb180f5883e232bdfe29df18386baa395
Jeremy Hon <jehon11@gmail.com>
2019-05-21 15:34:07 +0900

    changed navbar to stateless component

--- app/javascript/restaurants/components/navbar.jsx
+++ app/javascript/restaurants/components/navbar.jsx
@@ -2,51 +2,46 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logoSmall from 'images/logo_small.svg';
 
-class Navbar extends Component {
-  loggedIn = () => {
-    const {
-      loggedIn, openLogInModal, openSignUpModal, logOut,
-    } = this.props;
-    if (loggedIn) {
-      return (
-        <div className="nav-bar-right">
-          <Link className="nav-link" to="/reservations">
-            My Reservations
-          </Link>
-          <div className="nav-link log-out" onClick={logOut}>
-            Log out
-          </div>
-        </div>
-      );
-    }
+const loggedIn = ({loggedIn, openLogInModal, openSignUpModal, logOut}) => {
+  if (loggedIn) {
     return (
       <div className="nav-bar-right">
-        <div className="nav-link log-in" onClick={openLogInModal}>
-          log in
-        </div>
-        <div className="nav-link sign-up" onClick={openSignUpModal}>
-          sign up
-        </div>
-      </div>
-    );
-  };
-
-  render() {
-    return (
-      <div className="nav-bar">
-        <div className="nav-bar-left">
-          <div>
-            <Link className="nav-link" to="/">
-              <img src={logoSmall} alt="small-logo" />
-            </Link>
-          </div>
-        </div>
-        <div className="nav-bar-right">
-          {this.loggedIn()}
+        <Link className="nav-link" to="/reservations">
+          My Reservations
+        </Link>
+        <div className="nav-link log-out" onClick={logOut}>
+          Log out
         </div>
       </div>
     );
   }
-}
+  return (
+    <div className="nav-bar-right">
+      <div className="nav-link log-in" onClick={openLogInModal}>
+        log in
+      </div>
+      <div className="nav-link sign-up" onClick={openSignUpModal}>
+        sign up
+      </div>
+    </div>
+  );
+};
+
+const Navbar = (props) => {
+  return (
+    <div className="nav-bar">
+      <div className="nav-bar-left">
+        <div>
+          <Link className="nav-link" to="/">
+            <img src={logoSmall} alt="small-logo" />
+          </Link>
+        </div>
+      </div>
+      <div className="nav-bar-right">
+        {loggedIn(props)}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
