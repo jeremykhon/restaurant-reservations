b3bd1836ea2b819dff8e1eeb78efa5318824a73e
Jeremy Hon <jehon11@gmail.com>
2019-05-22 16:55:12 +0900

    added price level to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_115512) do
+ActiveRecord::Schema.define(version: 2019_05_21_112646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_115512) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "avg_rating", default: 0.0
+    t.integer "price_level"
     t.index ["cuisine_id"], name: "index_restaurants_on_cuisine_id"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
