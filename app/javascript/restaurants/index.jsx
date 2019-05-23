981c34271917cc3ef77ef510efa7089fa7bf33f2
Jeremy Hon <jehon11@gmail.com>
2019-05-23 16:21:53 +0900

    extracted css from react date picker

--- app/javascript/restaurants/index.jsx
+++ app/javascript/restaurants/index.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import 'react-datepicker/dist/react-datepicker.css';
 
 // internal modules
 const root = document.getElementById('root');
