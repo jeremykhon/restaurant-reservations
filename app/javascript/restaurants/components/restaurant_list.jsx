a2b5768bfe6c4ff6d47583a1cdd0c8e863039c41
Jeremy Hon <jehon11@gmail.com>
2019-05-27 17:11:44 +0900

    added restaurant card when loading

--- app/javascript/restaurants/components/restaurant_list.jsx
+++ app/javascript/restaurants/components/restaurant_list.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import axios from "axios";
-import Restaurant from './restaurant_card';
+import axios from 'axios';
+import RestaurantCard from './restaurant_card';
+import RestaurantLoadingCard from './restaurant_loading_card';
 import BASE_URL from '../utils/base_url';
 
 class RestaurantList extends Component {
@@ -13,14 +14,22 @@ class RestaurantList extends Component {
 
   componentDidMount() {
     axios.get(`${BASE_URL}/restaurants`)
-      .then(response => this.setState({ restaurants: response.data }));
+      .then(response => this.setState({ restaurants: response.data }))
+      .catch(error => console.log(error));
   }
 
   render() {
     const { restaurants } = this.state;
+    if (restaurants.length === 0) {
+      return (
+        <div className="row">
+          {[...Array(12).keys()].map(() => <RestaurantLoadingCard />)}
+        </div>
+      );
+    }
     return (
       <div className="row">
-        {restaurants.map(restaurant => <Restaurant key={restaurant.id} restaurant={restaurant} />)}
+        {restaurants.map(restaurant => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)}
       </div>
     );
   }
---
+++ app/javascript/restaurants/components/restaurant_loading_card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const RestaurantLoadingCard = () => {
+  return (
+    <div className="col-12 col-sm-3" style={{ padding: '0 10px' }}>
+      <div className="restaurant-loading">
+        <div className="restaurant-loading-card-image" />
+        <div className="restaurant-loading-card-content">
+          <div className="loading-content-1" />
+          <div className="loading-content-2" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantLoadingCard;
