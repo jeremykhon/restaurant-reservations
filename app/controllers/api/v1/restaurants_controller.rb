a652646a581ffa78e64178e99c78001dd32bbb56
Jeremy Hon <jehon11@gmail.com>
2019-06-03 18:55:43 +0900

    changed restaurants controller variables to instance variables

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::RestaurantsController < ApplicationController
   skip_before_action :authenticate_request, only: %i[index show]
 
   def index
-    restaurants = Restaurant.all
-    render json: restaurants, include: %i[restaurant_photos cuisine]
+    @restaurants = Restaurant.all
+    render json: @restaurants, include: %i[restaurant_photos cuisine]
   end
 
   def create
@@ -11,7 +11,7 @@ class Api::V1::RestaurantsController < ApplicationController
   end
 
   def show
-    restaurant = Restaurant.find_by!(id: params[:id])
-    render json: restaurant, include: [:restaurant_photos, :cuisine, reviews: { include: [user: { only: :name }] }]
+    @restaurant = Restaurant.find_by!(id: params[:id])
+    render json: @restaurant, include: [:restaurant_photos, :cuisine, reviews: { include: [user: { only: :name }] }]
   end
 end
