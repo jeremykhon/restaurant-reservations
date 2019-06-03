05a7bfb031742afdf17b3de02b8f5764f79a464e
Jeremy Hon <jehon11@gmail.com>
2019-06-03 15:52:07 +0900

    created tests for negative days and is date object

--- test/javascript/restaurants/utils/add_days.test.js
+++ test/javascript/restaurants/utils/add_days.test.js
@@ -1,10 +1,22 @@
 import addDays from 'restaurants/utils/add_days';
 
 describe('#addDays', () => {
-  it('returns the correct date', () => {
-    const date = new Date('2019-06-01');
+  const date = new Date('2019-06-01');
+
+  it('returns correct date', () => {  
     const receivedDate = addDays(date, 2);
     const expectedDate = new Date('2019-06-03');
     expect(receivedDate).toEqual(expectedDate);
+  });
+
+  it('returns correct date when days is negative', () => {
+    const receivedDate = addDays(date, -2);
+    const expectedDate = new Date('2019-05-30');
+    expect(receivedDate).toEqual(expectedDate);
+  });
+
+  it('returns a date object', () => {
+    const receivedDate = addDays(date, 2);
+    expect(receivedDate).toBeInstanceOf(Date);
   });
 });
